a76bdfedc3d245acc56b3b5d7f90ee4d7891091c
Stephen Anderson <anderssj@usc.edu>
2019-01-18 08:57:30 -0800

    display

--- startrek.js
+++ startrek.js
@@ -1,46 +1,47 @@
-'use strict';
+'use strict'
 // Create node, makes this a linked list
 class _Node {
   constructor(data, next) {
-    this.data = data;
-    this.next = next;
+    this.data = data
+    this.next = next
   }
 }
 class _StarTrek {
   constructor(top) {
-    this.top = null;
+    this.top = null
   }
 
   push(data) {
     // if this.top is null, the stack is empty b/c the constructor sets this.top=null as default. Then, the new node becomes the top, and last item in stack (next === null)
     if (this.top === null) {
-      this.top = new _Node(data, null);
-      return this.top;
+      this.top = new _Node(data, null)
+      return this.top
     }
     // if the stack is not empty, this.top !==null, then the new item becomes the new top, and it's next pointer points to the previous item, now 'below' it in the stack.
-    this.top = new _Node(data, this.top);
+    this.top = new _Node(data, this.top)
   }
   pop(data) {
-    const node = this.top;
-    this.top = node.next;
-    return node.data;
+    const node = this.top
+    this.top = node.next
+    return node.data
   }
-//   peek() {
-//     const node = this.top;
-//     return node.value;
-//   }
+  //   peek() {
+  //     const node = this.top;
+  //     return node.value;
+  //   }
 }
 
 function isEmpty(stack) {
   //helper function to check if stack is empty
-  let top = stack.top;
-  if (!top) return true;
-  else return false;
+  let top = stack.top
+  if (!top) return true
+  else return false
 }
 function display(stack) {
+  let copy = stack
   //to display the stack
-  while (!isEmpty(stack)) {
-    console.log(stack.pop());
+  while (!isEmpty(copy)) {
+    console.log(copy.pop())
   }
 }
 
@@ -50,19 +51,20 @@ function display(stack) {
 // }
 
 function main() {
-  let st = new _StarTrek();
+  let st = new _StarTrek()
 
-  st.push('Captain Kirk');
-  st.push('Scotty');
+  st.push('Captain Kirk')
+  st.push('Scotty')
   //   st.push('Spock');
   //   st.push('McCoy');
   //   st.pop();
-  st.peek();
-  display(st);
-  console.log(JSON.stringify(st.peek()));
+  // st.peek()
+  display(st)
+  console.log(isEmpty(st))
+  // console.log(JSON.stringify(st.peek()))
 }
 
-main();
+main()
 
 // Using the stack class above, implement the following helper functions outside the stack class
 // peek: allows you to look at the top of the stack without removing it
